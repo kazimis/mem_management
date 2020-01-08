9e3b992535d0d9916916463f2cb48ff782f4254d
kazimis <sarajkazimi@gmail.com>
2020-01-08 03:37:05 -0800

    implemeted functions

--- mymem.c
+++ mymem.c
@@ -35,26 +35,78 @@ static struct memoryList *next;
   memory after memory allocation
 */
 void update(struct memoryList* mem_block, size_t requested){
+  mem_block->alloc = 1;
+  if(mem_block->size > requested){
+    struct memoryList* remainder_mem = malloc(sizeof(struct memoryList));
+    if(mem_block -> next){
+      remainder_mem->next = mem_block->next;
+      mem_block->next->prev = remainder_mem;
+    }else{
+      remainder_mem->next = NULL;
+    }
+    remainder_mem->prev = mem_block;
+    mem_block->next = remainder_mem;
 
+    //update size and alloc and location in memory pool
+    remainder_mem->size = mem_block->size - requested;
+    remainder_mem->alloc = 0;
+    remainder_mem->ptr = mem_block->ptr + requested;
+    mem_block->size = requested;
+    next = remainder_mem;
+  }else {
+    if(mem_block->next){
+      next = mem_block->next;
+    }else{
+      next = head;
+    }
+  }
 }//end of update
 
 
 
 /*function to join the current block with left empty block*/
 void join_left(struct memoryList* current_block){
- 
+  struct memoryList* prev_block = current_block->prev;
+
+  prev_block->next = current_block->next;
+  if(current_block->next){
+    current_block->next->prev = prev_block;
+  }
+  prev_block->size += current_block->size;
+
+  if(next == current_block) {
+    next = prev_block;
+  }
+  free(current_block);
 }
 
 
 /*function to join the current block with right empty block*/
 void join_right(struct memoryList* current_block){
+  struct memoryList* next_block = current_block->next;
 
+  current_block->next = next_block->next;
+  if(next_block->next){
+    next_block->next->prev = current_block;
+  }
+  current_block->size += next_block->size;
+  if(next == next_block) {
+    next = current_block;
+  }
+  free(next_block);
 }
 
 
 //first_fit_memory request, return NULL if no memory is available
 void* first_fit_req(size_t requested) {
-
+    struct memoryList* tmp = head;
+    while(tmp){
+      if(!(tmp->alloc) && tmp->size >= requested){
+        update(tmp, requested);
+        return tmp->ptr;
+      }
+      tmp = tmp->next;
+    }
   return NULL;
 }//end of first_fit_memory
 
@@ -64,7 +116,23 @@ void* first_fit_req(size_t requested) {
   available memory, returns NULL otherwise
 */
 void* best_fit_req(size_t requested) {
+  struct memoryList* min_memory = NULL; //the smallest suitable memory
+  struct memoryList* tmp = head;
+  while(tmp){
+      if(!(tmp->alloc) && (tmp->size >= requested)){
+        if(!min_memory){
+          min_memory = tmp;
+        }else if(tmp->size < min_memory->size){
+          min_memory = tmp;
+        }
+      }
+      tmp = tmp->next;
+  }
 
+  if(min_memory){
+    update(min_memory,requested);
+    return min_memory->ptr;
+  }
   return NULL;
 }//end of best_fit_req
 
@@ -75,9 +143,25 @@ void* best_fit_req(size_t requested) {
 */
 
 void* worst_fit_req(size_t requested) {
+  struct memoryList* max_memory = NULL;
+  struct memoryList* tmp = head;
+  while(tmp){
+    if(!(tmp->alloc) && (tmp->size >= requested)){
+      if(!max_memory){
+        max_memory = tmp;
+      }else if(tmp->size > max_memory->size){
+        max_memory = tmp;
+      }
+    }
+    tmp = tmp->next;
+  }
 
+  if(max_memory){
+    update(max_memory,requested);
+    return max_memory->ptr;
+  }else{
     return NULL;
-  
+  }
 }//end of worst_fit_req
 
 
@@ -85,7 +169,18 @@ void* worst_fit_req(size_t requested) {
 after the last allocated memory, returns NULL otherwise
 */
 void* next_fit_req(size_t requested) {
-
+  struct memoryList* tmp = next;
+  int done = 0;
+  while(!done){
+    if(!(tmp->alloc) && tmp->size >= requested){
+      update(tmp, requested);
+      return tmp->ptr;
+    }
+    if(!(tmp = tmp->next)){
+      tmp=head;
+    }
+    done = (tmp == next);
+  }
 
   return NULL;
 }
@@ -106,6 +201,27 @@ void* next_fit_req(size_t requested) {
 
 void initmem(strategies strategy, size_t sz)
 {
+	myStrategy = strategy;
+
+	/* all implementations will need an actual block of memory to use */
+	mySize = sz;
+
+	if (myMemory != NULL) free(myMemory); /* in case this is not the first time initmem2 is called */
+
+	/* TODO: release any other memory you were using for bookkeeping when doing a re-initialization! */
+  if(head != NULL)
+    free(head);
+
+	myMemory = malloc(sz);
+
+	/* TODO: Initialize memory management structure. */
+  head = malloc(sizeof(struct memoryList));
+  head->size = sz;
+  head->alloc = 0;
+  head->ptr = myMemory;
+  head->next = NULL;
+  head->prev = NULL;
+  next = head;
 }
 
 /* Allocate a block of memory with the requested size.
@@ -116,13 +232,41 @@ void initmem(strategies strategy, size_t sz)
 
 void *mymalloc(size_t requested)
 {
-
+	assert((int)myStrategy > 0);
+	switch (myStrategy)
+	  {
+	  case NotSet:
+	            return NULL;
+	  case First:
+              return (first_fit_req(requested));
+	  case Best:
+              return best_fit_req(requested);
+	  case Worst:
+	            return worst_fit_req(requested);
+	  case Next:
+              return next_fit_req(requested);
+	  }
+	return NULL;
 }
 
 
 /* Frees a block of memory previously allocated by mymalloc. */
 void myfree(void* block){
-
+  struct memoryList* tmp = head;
+  while(tmp){
+    if(tmp->ptr == block){
+      break;
+    }
+    tmp = tmp->next;
+  }
+  if(tmp){
+    tmp->alloc = 0;
+    if(tmp!= head && !(tmp->prev->alloc)){
+      join_left(tmp);
+    }else if((tmp->next) && !(tmp->next->alloc)){
+      join_right(tmp);
+    }
+  }
 }
 
 /****** Memory status/property functions ******
@@ -133,32 +277,74 @@ void myfree(void* block){
 
 /* Get the number of contiguous areas of free space in memory. */
 int mem_holes(){
- return 0;
+  int count = 0;
+  struct memoryList* tmp = head;
+  while(tmp){
+    count += !tmp->alloc;
+    tmp = tmp->next;
+  }
+  return count;
 }
 
 /* Get the number of bytes allocated */
 int mem_allocated() {
-  return 0;
+  return mySize - mem_free();
 }
 
 /* Number of non-allocated bytes */
 int mem_free() {
-  return 0;
+  int count = 0;
+  struct memoryList* tmp = head;
+  while(tmp){
+    if(!tmp->alloc){
+      count+= tmp->size;
+    }
+    tmp = tmp->next;
+  }
+  return count;
 }
 
 /* Number of bytes in the largest contiguous area of unallocated memory */
 int mem_largest_free() {
-  return 0;
+  int largest_free = 0;
+  struct memoryList* tmp = head;
+  while(tmp){
+    if(!(tmp->alloc) && (tmp->size > largest_free)){
+      largest_free = tmp->size;
+    }
+    tmp = tmp->next;
+  }
+  return largest_free;
 }
 
 /* Number of free blocks smaller than "size" bytes. */
 int mem_small_free(int size) {
-  
-  return 0;
+  int count = 0;
+  struct memoryList* tmp = head;
+  while(tmp){
+    if(!(tmp->alloc) && (tmp->size <= size)){
+      count++;
+    }
+    tmp = tmp->next;
+  }
+  return count;
 }
 
 char mem_is_alloc(void *ptr){
- return 'a';
+  if(ptr >= myMemory && ptr <= mySize){
+    struct memoryList* tmp = head->next;
+    while(tmp){
+      if(!tmp->next){
+        return tmp->alloc;
+      }else{
+        if(ptr < tmp) {
+          return tmp->alloc;
+        }
+      }
+      tmp = tmp->next;
+      return tmp->alloc;
+    }
+  }
 }
 
 /*
@@ -169,13 +355,13 @@ char mem_is_alloc(void *ptr){
 //Returns a pointer to the memory pool.
 void *mem_pool()
 {
-	return NULL;
+	return myMemory;
 }
 
 // Returns the total number of bytes in the memory pool. */
 int mem_total()
 {
-	return 0;
+	return mySize;
 }
 
 
